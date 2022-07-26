85e5a57fa455ec4ab8f855941b23917d2ea8f4ef
Vinicius Pereira <viniciuspereiracamposvpc@gmail.com>
2022-07-25 21:23:14 -0300

    Commit 25/07/2022
    Primeiro Commit do projeto.

---
+++ Projeto-React/formulario/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import './Formulario.css';
+import './Main.css';
+import Main from './Main';
+import Formulario from './Formulario';
+
+function App(){
+  return(
+      <div className='App'>
+          <Main />
+          <Formulario />
+
+       </div>
+  );
+}
+
+export default App;
---
+++ Projeto-React/formulario/src/Main.js
@@ -0,0 +1,30 @@
+
+function Main(){
+    return(
+      <div className='main'>
+      <div className='center'>
+        <div className='menu'>
+          <div className='logo'>
+            <h2>Projeto.Teste</h2>
+          </div>
+        <div className='item-menu'>
+          <a href='#'><strong>Entrar</strong></a>
+        </div>
+        </div>
+        <div className="navegar">
+          <ul id="navigation">
+            <li><a href="">Home</a></li>
+            <li><a href="">Formulario</a></li>
+            <li><a href="">Clientes</a></li>
+            <li><a href="">Noticias</a></li>                                    
+            <li><a href="">Locações</a></li>
+            <li><a href="">Relatorios</a></li>
+            <li><a href="">Contato</a></li>
+          </ul>
+        </div>
+    </div>
+    </div>
+    )
+}
+
+export default Main;
